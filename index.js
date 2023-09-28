f67dc439241bdea7e5763dc1709cda6ae60f7b12
Koverin <110396064+Koverin@users.noreply.github.com>
2023-09-29 02:17:01 +0300

    well done jobs

--- index.js
+++ index.js
@@ -1,33 +1,73 @@
-const swiper = new Swiper(".swiper", {
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-    dinamicBullets: true,
-  },
+document.addEventListener("DOMContentLoaded", function () {
+  const form = document.forms.registration;
+  const phoneInput = form.elements.phone;
 
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
+  const iti = window.intlTelInput(phoneInput, {
+    initialCountry: "pl",
+    separateDialCode: true,
+    utilsScript:
+      "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.12/js/utils.js",
+  });
 
-  scrollbar: {
-    el: ".swiper-scrollbar",
-  },
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    const firstName = form.elements.first_name.value;
+    const lastName = form.elements.last_name.value;
+    const email = form.elements.email.value;
+    const phone = iti.getNumber();
 
-  grabCursor: true,
+    if (!firstName || !lastName || !email || !phone) {
+      alert("Please fill in all fields for Form .");
+    } else if (
+      !/^[A-Za-zżźćńółęąśŻŹĆĄŚĘŁÓŃ\s-]+$/.test(firstName) ||
+      !/^[A-Za-zżźćńółęąśŻŹĆĄŚŚŁÓŃ\s-]+$/.test(lastName)
+    ) {
+      alert(
+        "First name and last name for Form  can only contain letters, spaces, and hyphens."
+      );
+    } else if (!/\S+@\S+\.\S+/.test(email)) {
+      alert("Please enter a valid email address for Form .");
+    } else if (!iti.isValidNumber()) {
+      alert("Please enter a valid phone number for Form .");
+    } else {
+      alert("Form  is valid. Submitting...");
+      form.submit();
+    }
+  });
 
-  keyboard: {
-    enabled: true,
-    onlyInViewport: true,
-    pageUpDown: true,
-  },
+  const form1 = document.forms.registration1;
+  const phoneInput1 = form1.elements.phone;
 
-  slidesPerGroup: 1,
-  centeredSlides: true,
-  loop: true,
+  const iti1 = window.intlTelInput(phoneInput1, {
+    initialCountry: "pl",
+    separateDialCode: true,
+    utilsScript:
+      "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.12/js/utils.js",
+  });
 
-  autoplay: {
-    delay: 3000,
-  },
+  form1.addEventListener("submit", function (event) {
+    event.preventDefault();
+    const firstName1 = form1.elements.first_name.value;
+    const lastName1 = form1.elements.last_name.value;
+    const email1 = form1.elements.email.value;
+    const phone1 = iti1.getNumber();
+
+    if (!firstName1 || !lastName1 || !email1 || !phone1) {
+      alert("Please fill in all fields for Form .");
+    } else if (
+      !/^[A-Za-zżźćńółęąśŻŹĆĄŚĘŁÓŃ\s-]+$/.test(firstName1) ||
+      !/^[A-Za-zżźćńółęąśŻŹĆĄŚŚŁÓŃ\s-]+$/.test(lastName1)
+    ) {
+      alert(
+        "First name and last name for Form  can only contain letters, spaces, and hyphens."
+      );
+    } else if (!/\S+@\S+\.\S+/.test(email1)) {
+      alert("Please enter a valid email address for Form .");
+    } else if (!iti1.isValidNumber()) {
+      alert("Please enter a valid phone number for Form .");
+    } else {
+      alert("Form  is valid. Submitting...");
+      form1.submit();
+    }
+  });
 });
-
--- js/slider.js
+++ js/slider.js
@@ -1,10 +1,29 @@
 document.addEventListener("DOMContentLoaded", function () {
-  document
-    .getElementById("custom-slider")
-    .addEventListener("input", function (evt) {
-      let value = evt.target.value;
-      document.getElementById("current-value").innerText = value;
-      document.getElementById("current-value").classList.add("active");
-      document.getElementById("current-value").style.left = `${value / 415}px`;
-    });
+  const customSlider = document.getElementById("custom-slider");
+  const currentValue = document.getElementById("current-value");
+
+  function updateSliderValue() {
+    const value = customSlider.value;
+    currentValue.innerText = value;
+    currentValue.classList.add("active");
+
+    const screenWidth = window.innerWidth;
+    let leftPosition;
+
+    if (screenWidth >= 768 && screenWidth <= 1399) {
+      leftPosition = `${value / 780}px`;
+    } else if (screenWidth >= 320 && screenWidth <= 767) {
+      leftPosition = `${value / 1460}px`;
+    } else {
+      leftPosition = `${value / 415}px`;
+    }
+
+    currentValue.style.left = leftPosition;
+  }
+
+  updateSliderValue();
+
+  customSlider.addEventListener("input", updateSliderValue);
+
+  window.addEventListener("resize", updateSliderValue);
 });
